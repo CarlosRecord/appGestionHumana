fa4453e00a8469b2e7ef2648511dd94b3732bf41
Carlos Rodriguez <carlos.rodriguez@record.com.co>
2021-08-21 08:55:12 -0500

    Se crea un nuevo test

--- test/models/type_vacation_test.rb
+++ test/models/type_vacation_test.rb
@@ -1,10 +1,17 @@
-require "test_helper"
+require 'test_helper'
 
 class TypeVacationTest < ActiveSupport::TestCase
 
-  test "No debe guardar tipo vacaciones sin descripción" do
+  test 'No debe guardar tipo vacaciones sin descripcion' do
     description = TypeVacation.new
-    assert_not description.save, "Se guardó el tipo vacaciones sin descripción."
+    assert_not description.save, 'Se guardó el tipo vacaciones sin descripción.'
   end
+
+  test 'No debe guardar tipo de vacaciones con descripcion muy corta' do
+    vacacion = TypeVacation.new
+    vacacion.description = "En lo que sea"
+    assert_not vacacion.save, 'El tipo de vacaion se guardó a pesar de ser muy corto.'
+  end
+
 
 end
