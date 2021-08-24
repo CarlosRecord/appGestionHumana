c955a1546ae3be54535611d5fde52e3bd595b18e
Carlos Rodriguez <carlos.rodriguez@record.com.co>
2021-08-24 15:55:36 -0500

    Se crea el CRUD completo

--- app/controllers/type_vacations_controller.rb
+++ app/controllers/type_vacations_controller.rb
@@ -1,5 +1,43 @@
 class TypeVacationsController < ApplicationController
     def index
-        @typeVacation = TypeVacation.all
+        @typeVacations = TypeVacation.all
     end
+
+    def new
+        @typeVacation = TypeVacation.new
+    end
+
+    def create
+        @typeVacation = TypeVacation.new(typeVacation_params)
+        if @typeVacation.save
+            redirect_to type_vacations_path, notice: "El libro ha sido creado exitosamente"
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @typeVacation = TypeVacation.find(params[:id])
+    end
+
+    def update
+        @typeVacation = TypeVacation.find(params[:id])
+        if @typeVacation.update(typeVacation_params)
+            redirect_to type_vacations_path, notice: "El libro ha sido actualizado exitosamente"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        typeVacation = TypeVacation.find(params[:id])
+        typeVacation.destroy
+
+        redirect_to type_vacations_path, notice: "El libro ha sido eliminado exitosamente"
+    end
+
+    private
+        def typeVacation_params
+            params.require(:type_vacation).permit(:description)
+        end
 end
