f836a5bb1bf969872d9b84e31864ab2f16de39b1
Carlos Rodriguez <carlos.rodriguez@record.com.co>
2021-09-27 08:30:40 -0500

    Se actualiza el link

--- app/models/directed_certificate.rb
+++ app/models/directed_certificate.rb
@@ -1,4 +1,5 @@
 class DirectedCertificate < ApplicationRecord
-  validates :description, presence: true
+  validates :description, presence: true, format: { with: /\A[a-z\sA-Z]+\z/,
+    message: "solo permite letras" }
   has_many :certificates
 end
